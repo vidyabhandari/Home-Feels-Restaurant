8598e286e2e188a810b0c4989c26ef0dfb0f6fb8
vidyabhandari <vidyabhandari2804@gmail.com>
2025-01-21 21:33:01 +0530

    conatct page added

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -9,39 +9,81 @@ import pintrest from "../assets/pintrest-svgrepo-com (1).svg";
 
 const ContactPage = () => {
   return (
-    <div className="flex items-center py-6 flex-col justify-center p-3">
-      <h1 className="text-[30px] font-[Playfair-Display]">Contact page</h1>
-      <div>
-        <div className="flex-col gap-32 p-2">
-          <div>
-            <img src={email} />
-            <p className="text-2xl font-[Inconsolata]">Email us</p>
-            <p>info@homefeels.com</p>
+    <div className="flex py-6 flex-col w-full">
+      <h1 className="text-[50px] font-semibold font-[Playfair-Display] text-center pb-5">
+        Contact Us
+      </h1>
+      <div className="w-full gap-20 flex items-center justify-evenly m-auto">
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-5 p-2">
+            <div className="flex gap-4 items-center">
+              <img src={email} />
+              <div>
+                <p className="text-2xl font-[Inconsolata]">Email us</p>
+                <p>info@homefeels.com</p>
+              </div>
+            </div>
+            <div className="flex gap-4 items-center">
+              <img src={phone} />
+              <div>
+                <p className="text-2xl font-[Inconsolata]">Phone</p>
+                <p>1234567890</p>
+              </div>
+            </div>
+            <div className="flex gap-4 items-center">
+              <img src={location} />
+              <div>
+                <p className="text-2xl font-[Inconsolata]">Location</p>
+                <p>Location,location</p>
+              </div>
+            </div>
           </div>
-          <div>
-            <img src={phone} />
-            <p className="text-2xl font-[Inconsolata]">Phone</p>
-            <p>1234567890</p>
-          </div>
-          <div>
-            <img src={location} />
-            <p className="text-2xl font-[Inconsolata]">Location</p>
-            <p>Location,location</p>
+          <div className="flex items-center gap-8 justify-center">
+            <img src={facebook} alt="" />
+            <img src={instagram} alt="" />
+            <img src={twitter} alt="" />
+            <img src={pintrest} alt="" />
           </div>
         </div>
-        <div className="flex gap-2">
-          <img src={facebook} alt="" />
-          <img src={instagram} alt="" />
-          <img src={twitter} alt="" />
-          <img src={pintrest} alt="" />
-        </div>
-      </div>
 
-      <div>
-        <label htmlFor="FirstName" id="First-Name">FirstName</label>
-        <input type="text" name="FirstName" id="FirstName"/>
-        <label htmlFor="LastName" id="Last-Name">LastName</label>
-        <input type="text" name="LastName" id="LastName" />
+        <form className="flex flex-col gap-3">
+          <div className="flex gap-1 flex-col">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name"
+              className="w-80 border-2 text-md px-2 py-1"
+            />
+          </div>
+          <div className="flex gap-1 flex-col">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              className="w-80 border-2 text-md px-2 py-1"
+            />
+          </div>
+          <div className="flex gap-1 flex-col">
+            <label htmlFor="message">Message</label>
+            <textarea
+              type="text"
+              name="message"
+              id="message"
+              placeholder="Message"
+              className="w-80 border-2 text-md px-2 py-1"
+            />
+          </div>
+
+          <div className="mt-3">
+            <button className="border-[#E27D60] m-auto px-2 py-2 border-2 w-full rounded-md text-[#E27D60] text-lg font-semibold">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div>
       <Hero />
       <AboutUsPage />
-      {/* <ContactPage /> */}
+      <ContactPage />
     </div>
   );
 };
