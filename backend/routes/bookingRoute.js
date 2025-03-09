e2bd3df9b2dfcef9f15500de7d753394db65b1da
vidyabhandari <vidyabhandari2804@gmail.com>
2025-03-09 11:45:44 +0530

    changes

--- backend/routes/bookingRoute.js
+++ backend/routes/bookingRoute.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { bookTable, verifyBookingPayment, listBookings, userBookings, updateBookingStatus, verifyBooking } from "../controllers/BookingController.js";
+import {
+  bookTable,
+  verifyBookingPayment,
+  listBookings,
+  userBookings,
+  updateBookingStatus,
+  verifyBooking,
+} from "../controllers/BookingController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,7 +14,7 @@ const router = express.Router();
 router.post("/book-table", authMiddleware, bookTable);
 router.post("/verify-payment", verifyBookingPayment);
 router.get("/list", listBookings);
-router.post("/user-bookings",authMiddleware, userBookings);
+router.post("/user-bookings", authMiddleware, userBookings);
 router.post("/update-status", updateBookingStatus);
 router.post("/verify-booking", verifyBooking);
 
