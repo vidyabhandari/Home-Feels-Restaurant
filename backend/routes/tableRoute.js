bff76c16f1bacf48dbd50bcdb9c4559524a3ca88
vidyabhandari <vidyabhandari2804@gmail.com>
2025-03-09 11:30:40 +0530

    table route updated

--- backend/routes/tableRoute.js
+++ backend/routes/tableRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAvailableTables } from "../controllers/TableController.js";
+import { getAvailableTables } from "../controllers/tableController.js";
 
 const router = express.Router();
 router.get("/", getAvailableTables);
